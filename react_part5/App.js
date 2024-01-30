73ba364f0ab9fd68a31b17a0e324fb0dc0afe4fd
rahulpannati513 <84844301+rahulpannati513@users.noreply.github.com>
2024-01-31 02:30:51 +0530

    react code

---
+++ react_part5/App.js
@@ -0,0 +1,20 @@
+//React.createElement -> it is a object becomes html that browser can understand
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm an h1 element"),
+    React.createElement("h2", {}, "I'm an h2 element"),
+  ]), //If you want to add more sibling elements, you can add them in an array
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm an h1 element"),
+    React.createElement("h2", {}, "I'm an h2 element"),
+  ]),
+]);
+// it is so much tedious to write this way, so we use JSX to write this in a more readable way and then we use babel to convert it to React.createElement
+// JSX is a syntactic sugar for React.createElement
+// JSX is not html, it is a javascript
+// JSX is a javascript expression that gets converted to React.createElement
+// JSX is a javascript expression that gets converted to React.createElement
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
